447daa90ea41040a9442328c5410b44b45cea5fb
Rogeriodelima <rogeriodelima@gmail.com>
2026-05-25 19:34:52 -0300

    fix: adiciona qa.codigodoasfalto.com.br em allowedDevOrigins

--- frontend-web/next.config.ts
+++ frontend-web/next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  allowedDevOrigins: ['2.24.71.43'],
+  allowedDevOrigins: ['2.24.71.43', 'qa.codigodoasfalto.com.br'],
   devIndicators: false,
 };
 
